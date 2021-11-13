8c4c3a6b275809a21237bcd21193f4e76f43a9d0
Maryam Aghababaei <m.aghababaei@irisad.net>
2021-11-13 15:48:49 +0330

    add images component

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import PersonelCard from './PersonelCard';
 import BMICard from './BMICard';
 import UploadAndDisplayImage from './UploadAndDisplayImage';
 
 function App(props) {
+  const [list, setList] = useState([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]);
   return (
     // <Grid container justify="center" style={{paddingTop:20}}>
     //   <Grid item style={{ padding: 5 }}>
@@ -14,7 +15,16 @@ function App(props) {
     //     <BMICard />
     //   </Grid>
     // </Grid>
-    <UploadAndDisplayImage/>
+
+    <Grid container justify="center" style={{ paddingTop: 30 }}>
+      {list.map((row) => {
+        return (
+          <Grid item style={{ padding: 5 }}>
+            <UploadAndDisplayImage id={row.id} />
+          </Grid>)
+      })}
+
+    </Grid>
   );
 }
 
--- src/component/UploadAndDisplayImage.js
+++ src/component/UploadAndDisplayImage.js
@@ -6,22 +6,17 @@ import ImageAdd from './image/imageAdd.png';
 
 
 const UploadAndDisplayImage = () => {
-  const [selectedImage1, setSelectedImage1] = useState(null);
-  const [selectedImage2, setSelectedImage2] = useState(null);
-  const [selectedImage3, setSelectedImage3] = useState(null);
-  const [selectedImage4, setSelectedImage4] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
   const [defultImage,] = useState(ImageAdd);
 
 
   return (
-    <Grid container justify="center" style={{ paddingTop: 30 }}>
-      <Grid item style={{ padding: 5 }}>
         <Card style={{ width: 200, paddingBottom: 10, textAlign: "center" }}>
           <CardContent >
             <Grid container justify="center" >
               <Grid item style={{ padding: 5 }}>
-                {selectedImage1 ?
-                  <img alt="not fount" width={"110px"} height={"110"} src={URL.createObjectURL(selectedImage1)} />
+                {selectedImage ?
+                  <img alt="not fount" width={"110px"} height={"110"} src={URL.createObjectURL(selectedImage)} />
                   : <img alt="not fount" width={"110px"} height={"110"} src={defultImage} />}
               </Grid>
               <Grid item style={{ paddingLeft: 70, paddingTop: 10 }}>
@@ -29,85 +24,13 @@ const UploadAndDisplayImage = () => {
                   type="file"
                   name="myImage"
                   onChange={(event) => {
-                    setSelectedImage1(event.target.files[0]);
+                    setSelectedImage(event.target.files[0]);
                   }}
                 />
               </Grid>
             </Grid>
           </CardContent>
         </Card>
-      </Grid>
-      <Grid item style={{ padding: 5 }}>
-        <Card style={{ width: 200, paddingBottom: 10, textAlign: "center" }}>
-          <CardContent >
-            <Grid container justify="center">
-              <Grid item style={{ padding: 5 }}>
-                {selectedImage2 ?
-                  <img alt="not fount" width={"110px"} height={"110"} src={URL.createObjectURL(selectedImage2)} />
-                  : <img alt="not fount" width={"110px"} height={"110"} src={defultImage} />}
-              </Grid>
-              <Grid item style={{ paddingLeft: 70, paddingTop: 10 }}>
-                <input
-                  type="file"
-                  name="myImage"
-                  onChange={(event) => {
-                    console.log(event.target.files[0]);
-                    setSelectedImage2(event.target.files[0]);
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item style={{ padding: 5 }}>
-        <Card style={{ width: 200, paddingBottom: 10, textAlign: "center" }}>
-          <CardContent >
-            <Grid container justify="center">
-              <Grid item style={{ padding: 5 }}>
-                {selectedImage3 ?
-                  <img alt="not fount" width={"110px"} height={"110"} src={URL.createObjectURL(selectedImage3)} />
-                  : <img alt="not fount" width={"110px"} height={"110"} src={defultImage} />}
-              </Grid>
-              <Grid item style={{ paddingLeft: 70, paddingTop: 10 }}>
-                <input
-                  type="file"
-                  name="myImage"
-                  onChange={(event) => {
-                    console.log(event.target.files[0]);
-                    setSelectedImage3(event.target.files[0]);
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item style={{ padding: 5 }}>
-        <Card style={{ width: 200, paddingBottom: 10, textAlign: "center" }}>
-          <CardContent >
-            <Grid container justify="center">
-              <Grid item style={{ padding: 5 }}>
-                {selectedImage4 ?
-                  <img alt="not fount" width={"110px"} height={"110"} src={URL.createObjectURL(selectedImage4)} />
-                  : <img alt="not fount" width={"110px"} height={"110"} src={defultImage} />
-                }
-              </Grid>
-              <Grid item style={{ paddingLeft: 70, paddingTop: 10 }}>
-                <input
-                  type="file"
-                  name="myImage"
-                  onChange={(event) => {
-                    console.log(event.target.files[0]);
-                    setSelectedImage4(event.target.files[0]);
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
   );
 };
 
